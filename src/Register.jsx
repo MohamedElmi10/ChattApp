850bccab5c146865a0642966d5bae0948901f76c
MohamedElmi10 <144214489+MohamedElmi10@users.noreply.github.com>
2024-08-26 12:25:19 +0200

    Done with css for Register component

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,9 +1,74 @@
 //imports
 import { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom";
-import Login from "./Login";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom"
 
-// Register component
+const Card = styled.div`
+
+    width: 330px;
+    height: 490px;
+    background-color: #000000;
+    cursor: pointer;
+    border-radius: 10px;
+    box-shadow: 0 0 12px 4px rgba(0,0,0,0.4);
+    overflow: hidden;
+    padding: 26px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    gap: 5px;
+    transition: all 0.5s ease-in-out;
+    &:hover {
+        transform: translateY(-10px);
+        box-shadow: 0 0 50px 4px rgba(0,0,0,0.6);
+        filter: brightness(1.1);
+    }
+    `;
+const Header = styled.p`
+    font-weight: 700;
+    line-height: 68px;
+    font-size: 46px;
+    color: #F2F3F4;
+`
+
+const Label = styled.label`
+    font-size: 20px;
+    font-weight: 500;
+    color: #ffff;
+    margin-bottom: 8px;
+    width: 50%;
+    
+ 
+`
+const FormGroup = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    gap:10px;
+    margin-bottom: 15px;
+`
+const Input = styled.input`
+  padding: 8px;
+  font-size: 15px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  width:100%;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  &:hover {
+    background-color: #45a049;
+  }
+`;
+
+
 const Register = () => {
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
@@ -106,29 +171,37 @@ const Register = () => {
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <h2>register</h2>
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    id="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <br />
-                <label htmlFor="UserName">Username</label>
-                <input
-                    type="text"
-                    id="UserName"
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <br />
-                <label htmlFor="PassWord">Password</label>
-                <input
-                    type="password"
-                    id="PassWord"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <br />
-                <button type="submit" >Register</button>
+                <Header>Register</Header>
+                <Card>
+                    <FormGroup>
+                        <Label htmlFor="email">Email</Label>
+                        <Input
+                            type="email"
+                            id="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Enter your email..."
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor="UserName">Username</Label>
+                        <Input
+                            type="text"
+                            id="UserName"
+                            onChange={(e) => setUsername(e.target.value)}
+                            placeholder="Enter your username..."
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor="PassWord">Password</Label>
+                        <Input
+                            type="password"
+                            id="PassWord"
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Enter your Password..."
+                        />
+                    </FormGroup>
+                    <Button type="submit" >Register</Button>
+                </Card>
             </form>
             {error && <p style={{ color: "red" }}>{error}</p>}
             {success && <p style={{ color: "Green" }}>{success}</p>}
